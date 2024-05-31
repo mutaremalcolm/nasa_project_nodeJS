5462bfa07df607ad017cc9f195444d585069384a
“malcolmmutare” <mutarem@gmail.com>
2024-05-31 12:00:00 +0200

    Implemented Morgan to enable event logging

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const  cors =  require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router'); 
 
@@ -8,7 +9,9 @@ const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000',
-}))
+}));
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname,  '..', 'public')))
 
@@ -17,4 +20,4 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
-module.exports = app;
+module.exports = app; 
