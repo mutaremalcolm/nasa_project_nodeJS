b241f8b262542083467d900d8d01084829a1c2fd
“malcolmmutare” <mutarem@gmail.com>
2024-07-11 22:47:17 +0200

    feat added to the MVC for better structure

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -34,8 +34,12 @@ function loadPlanetsData() {
         });
     }
 
+function getAllPlanets() {
+    return habitablePlanets;
+}
+
 module.exports = {
             loadPlanetsData,
-            planets: habitablePlanets
+            getAllPlanets
         }
 
---
+++ server/src/routes/launches/launches.controller.js
@@ -0,0 +1,9 @@
+const launchesModel = require('../../models/launches.model');
+
+function httpGetAllLaunches(req, res) {
+    return res.status(200).json(launchesModel.getAllLaunches());
+}
+
+module.exports = {
+    httpGetAllLaunches,
+};
